d85b17654ecc88697f379c66d206e3183ba3165e
Kreattehak <nerokamil@gmail.com>
2018-02-08 18:49:51 +0100

    Local variable rename, code decorations

--- src/main/java/pl/home/MusicShuffler.java
+++ src/main/java/pl/home/MusicShuffler.java
@@ -20,41 +20,21 @@ public class MusicShuffler {
 
     private FileNameCleaner fileNameCleaner;
     private PrefixShuffler prefixShuffler;
+    private UserInputChecker userInputChecker;
 
     MusicShuffler() {
         this.fileNameCleaner = new FileNameCleaner();
         this.prefixShuffler = new PrefixShuffler();
+        this.userInputChecker = new UserInputChecker();
     }
 
     void shuffle(String[] args) {
-        checkIfUserInputIsCorrect(args);
+        folderWithMusic = userInputChecker.checkIfUserInputIsCorrect(args);
         System.out.println("Provided path: " + folderWithMusic.toAbsolutePath());
 
         cleanNamesAndInitializeShuffle();
         shuffleFileNames();
         System.out.println("Files shuffled successfully.");
-    }
-
-    private void checkIfUserInputIsCorrect(String[] args) throws IllegalArgumentException {
-        if (inputOnlyContainsOneArgument(args)) {
-            checkIfGivenPathLeadsToAFolder(args[0]);
-        } else {
-            throw new IllegalArgumentException("Please provide only one cmd arg" +
-                    " which is a path to folder with your music!");
-        }
-    }
-
-    private boolean inputOnlyContainsOneArgument(String[] args) {
-        return args.length == 1;
-    }
-
-    private void checkIfGivenPathLeadsToAFolder(String path) throws IllegalArgumentException {
-        File file = new File(path);
-        if (file.isDirectory()) {
-            folderWithMusic = file.toPath();
-            return;
-        }
-        throw new IllegalArgumentException("Please provide a proper path to folder with your music!");
     }
 
     private void cleanNamesAndInitializeShuffle() {
@@ -73,11 +53,11 @@ public class MusicShuffler {
 
     private void checkIfMusicWasAlreadyShuffled(Path folder) {
         try {
-            List<Path> dirty = Files.walk(folder, 1)
+            List<Path> dirtyFileNames = Files.walk(folder, 1)
                     .filter(path -> Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS))
                     .collect(Collectors.toList());
 
-            dirty.forEach(data -> fileNameCleaner.cleanAndGetFileNames(data,
+            dirtyFileNames.forEach(data -> fileNameCleaner.cleanAndGetFileNames(data,
                     fileNamesWithoutShufflePrefix, shuffledFileNames));
         } catch (IOException e) {
             e.printStackTrace();
